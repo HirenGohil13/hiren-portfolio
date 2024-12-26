b6cb9d42e01e8a2482a008a06812bfdc074c9a31
HirenGohi13 <hiren13190@gmail.com>
2024-12-26 13:09:28 +0530

    delete all files

--- src/components/shared/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as Badge } from './Badge';
-export { default as Button } from './Button';
-export { default as Card } from './Card';
-export { default as Gradient } from './Gradient';
-export { default as StatCard } from './StatCard';
